def6830c89a0b5c110bc10beddfa4e5f0078f61d
袁军平 <1924188282@qq.com>
2022-05-19 18:02:03 +0800

    first

---
+++ configuration/config.py
@@ -0,0 +1,71 @@
+import argparse
+
+
+
+
+
+
+def base_parser():
+    parser = argparse.ArgumentParser(description='KNN-Softmax Training')
+
+    # hype-parameters
+    parser.add_argument('-lr', type=float, default=1e-4,
+                        help="learning rate of new parameters")
+    parser.add_argument('-tradeoff', type=float, default=1.0,
+                        help="learning rate of new parameters")
+    parser.add_argument('-exp', type=str, default='exp1',
+                        help="learning rate of new parameters")
+    parser.add_argument('-margin', type=float, default=0.0,
+                        help="margin for metric loss")
+
+    parser.add_argument('-BatchSize', '-b', default=128, type=int, metavar='N',
+                        help='mini-batch size (1 = pure stochastic) Default: 256')
+    parser.add_argument('-num_instances', default=8, type=int, metavar='n',
+                        help=' number of samples from one class in mini-batch')
+    parser.add_argument('-dim', default=512, type=int, metavar='n',
+                        help='dimension of embedding space')
+    parser.add_argument('-alpha', default=30, type=int, metavar='n',
+                        help='hyper parameter in KNN Softmax')
+    parser.add_argument('-k', default=16, type=int, metavar='n',
+                        help='number of neighbour points in KNN')
+
+    # network
+    parser.add_argument('-data', default='cifar100', required=False,
+                        help='path to Data Set')
+    parser.add_argument('-net', default='resnet18')
+    parser.add_argument('-loss', default='triplet_no_hard_mining', required=False,
+                        help='loss for training network')
+    parser.add_argument('-epochs', default=200, type=int, metavar='N',
+                        help='epochs for training process')
+
+    parser.add_argument('-seed', default=1993, type=int, metavar='N',
+                        help='seeds for training process')
+    parser.add_argument('-save_step', default=50, type=int, metavar='N',
+                        help='number of epochs to save model')
+    parser.add_argument('-lr_step', default=200, type=int, metavar='N',
+                        help='number of epochs to save model')
+    # Resume from checkpoint
+    parser.add_argument('-start', default=0, type=int,
+                        help='resume epoch')
+
+    # basic parameter
+    parser.add_argument('-log_dir', default='cifar100',
+                        help='where the trained models save')
+    parser.add_argument('--nThreads', '-j', default=2, type=int, metavar='N',
+                        help='number of data loading threads (default: 2)')
+    parser.add_argument('--momentum', type=float, default=0.9)
+    parser.add_argument('--weight-decay', type=float, default=2e-4)
+    parser.add_argument("-gpu", type=str, default='0',
+                        help='which gpu to choose')
+    parser.add_argument("-method", type=str,
+                        default='Finetuning', help='Choose FT or SC')
+
+    parser.add_argument('-mapping_mean', default='no',
+                        type=str, help='mapping')
+    parser.add_argument('-sigma', default=0.0, type=float, help='sigma')
+    parser.add_argument('-vez', default=0, type=int, help='vez')
+    parser.add_argument('-task', default=11, type=int, help='vez')
+    parser.add_argument('-base', default=50, type=int, help='vez')
+
+    args = parser.parse_args()
+    return args
---
+++ main.py
@@ -0,0 +1,193 @@
+import logging.config
+import os
+import random
+
+import numpy as np
+import torch
+import torch.nn as nn
+from torchvision import transforms
+
+from configuration import config
+from utils.CIFAR100 import CIFAR100
+from utils.ImageFolder import ImageFolder
+from utils.data_loader import get_statistics
+from methods.finetune import Finetune
+from collections import defaultdict
+from dataset import get_datalist, get_classlist
+from utils.sampler import RandomIdentitySampler
+
+args = config.base_parser()
+
+
+def getData():    # Data
+    print('==> Preparing data..')
+
+    if args.data == "cifar100":
+        transform_train = transforms.Compose([
+            transforms.RandomCrop(32, padding=4),
+            transforms.RandomHorizontalFlip(),
+            transforms.ToTensor(),
+            transforms.Normalize((0.4914, 0.4822, 0.4465),
+                                 (0.2023, 0.1994, 0.2010)),
+        ])
+        root = 'DataSet'
+        traindir = root + '/cifar'
+        num_classes = 100
+
+    if args.data == 'cub':
+        mean_values = [0.485, 0.456, 0.406]
+        std_values = [0.229, 0.224, 0.225]
+        transform_train = transforms.Compose([
+            transforms.Resize(256),
+            transforms.RandomResizedCrop(224),
+            transforms.RandomHorizontalFlip(),
+            transforms.ToTensor(),
+            transforms.Normalize(mean=mean_values,
+                                 std=std_values)
+        ])
+        transform_test = transforms.Compose([
+            transforms.Resize(256),
+            transforms.CenterCrop(224),
+            transforms.ToTensor(),
+            transforms.Normalize(mean=mean_values,
+                                 std=std_values),
+        ])
+        root = 'DataSet/CUB_200_2011'
+        traindir = os.path.join(root, 'train')
+        testdir = os.path.join(root, 'test')
+
+        num_classes = 200
+
+    if args.data == 'car':
+        mean_values = [0.485, 0.456, 0.406]
+        std_values = [0.229, 0.224, 0.225]
+        transform_train = transforms.Compose([
+            transforms.Resize(256),
+            transforms.RandomResizedCrop(224),
+            transforms.RandomHorizontalFlip(),
+            transforms.ToTensor(),
+            transforms.Normalize(mean=mean_values,
+                                 std=std_values)
+        ])
+        transform_test = transforms.Compose([
+            transforms.Resize(256),
+            transforms.CenterCrop(224),
+            transforms.ToTensor(),
+            transforms.Normalize(mean=mean_values,
+                                 std=std_values),
+        ])
+        root = 'DataSet/Car196'
+        traindir = os.path.join(root, 'train')
+        testdir = os.path.join(root, 'test')
+
+        num_classes = 196
+
+    if args.data == 'flower':
+        mean_values = [0.485, 0.456, 0.406]
+        std_values = [0.229, 0.224, 0.225]
+        transform_train = transforms.Compose([
+            transforms.Resize(256),
+            transforms.RandomResizedCrop(224),
+            transforms.RandomHorizontalFlip(),
+            transforms.ToTensor(),
+            transforms.Normalize(mean=mean_values,
+                                 std=std_values)
+        ])
+        transform_test = transforms.Compose([
+            transforms.Resize(256),
+            transforms.CenterCrop(224),
+            transforms.ToTensor(),
+            transforms.Normalize(mean=mean_values,
+                                 std=std_values),
+        ])
+        root = 'DataSet/flowers'
+        traindir = os.path.join(root, 'train')
+        testdir = os.path.join(root, 'test')
+
+        num_classes = 102
+
+    if args.data == 'imagenet_sub' or args.data == 'imagenet_full':
+        mean_values = [0.485, 0.456, 0.406]
+        std_values = [0.229, 0.224, 0.225]
+        transform_train = transforms.Compose([
+            # transforms.Resize(256),
+            transforms.RandomResizedCrop(224),
+            transforms.RandomHorizontalFlip(),
+            transforms.ToTensor(),
+            transforms.Normalize(mean=mean_values,
+                                 std=std_values)
+        ])
+        root = '/datatmp/datasets/ILSVRC12_256'
+        traindir = os.path.join(root, 'train')
+        num_classes = 100
+    return transform_train,traindir,num_classes
+
+
+def main():
+
+
+
+    # logging.config.fileConfig("./configuration/logging.conf")
+    # logger = logging.getLogger()
+    #
+    # os.makedirs(f"logs/{args.dataset}", exist_ok=True)
+    # save_path = "tmp"
+    # fileHandler = logging.FileHandler("logs/{}.log".format(save_path), mode="w")
+    # formatter = logging.Formatter(
+    #     "[%(levelname)s] %(filename)s:%(lineno)d > %(message)s"
+    # )
+    # fileHandler.setFormatter(formatter)
+    # logger.addHandler(fileHandler)
+    #
+    #
+    # if torch.cuda.is_available():
+    #     device = torch.device("cuda")
+    # else:
+    #     device = torch.device("cpu")
+    #
+    #     logger.info(f"Set the device ({device})")
+
+    transform_train,traindir,num_classes=getData()
+
+    num_task = args.task
+    num_class_per_task = (num_classes - args.base) // (num_task - 1)
+
+    np.random.seed(args.seed)
+    random_perm = np.random.permutation(num_classes)
+    os.environ['CUDA_VISIBLE_DEVICES'] = args.gpu
+    fisher = {}
+    prototype = {}
+    for i in range(num_task):
+        if i == 0:
+            class_index = random_perm[:args.base]
+        else:
+            class_index = random_perm[args.base + (i - 1) * num_class_per_task:args.base + i * num_class_per_task]
+        if args.data == 'cifar100':
+            trainfolder = CIFAR100(
+                root=traindir, train=True, download=True, transform=transform_train, index=class_index)
+        else:
+            trainfolder = ImageFolder(
+                traindir, transform_train, index=class_index)
+
+        train_loader = torch.utils.data.DataLoader(
+            trainfolder, batch_size=args.BatchSize,
+            sampler=RandomIdentitySampler(
+                trainfolder, num_instances=args.num_instances),
+            drop_last=True, num_workers=args.nThreads)
+
+        feat_loader = torch.utils.data.DataLoader(
+            trainfolder, batch_size=1, shuffle=True, drop_last=False)
+        # Fix the random seed to be sure we have the same permutation for one experiment
+
+        #这里调用训练的接口
+
+        # if args.method == 'EWC' or args.method == 'MAS':
+        #     fisher = train_fun(args, train_loader,
+        #                        feat_loader, i, fisher=fisher)
+        # else:
+        #     train_fun(args, train_loader, feat_loader, i)
+
+
+
+if __name__ == "__main__":
+    main()
